64583b4e4dfb7b956c2e1462b9584e25e4fc1f2b
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-12-25 02:55:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1648,7 +1648,7 @@ msg.channel.send(embed).then(() => {
 
 client.on('ready', function(){	
     var ms = 40000 ;	
-    var setGame = ['-inv','-help'];	
+    var setGame = ['by Fras#9999','-help'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
@@ -2429,17 +2429,7 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 
-client.on('message', message => {
-        if (message.content === "-inv") {
-            if(!message.channel.guild) return;
-        let embed = new Discord.RichEmbed()
-        .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
-        .setTitle(`:small_orange_diamond: اضغط هنا `)
-        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=466256531998900246&permissions=8&scope=bot`)
-        .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
-     message.channel.sendEmbed(embed);
-       }
-   });
+
  
 
 client.on('message', message => {
